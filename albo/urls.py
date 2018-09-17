d8b1ac0902de4d80a3fc489394468803d8fedf62
Tommaso A <tommaso.amici@me.com>
2018-09-17 20:12:27 +0200

    added favicon

--- albo/urls.py
+++ albo/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('squadre/', views.squadre, name='squadre'),
-    path('squadra/<int:pk>', views.team_detail, name='squadra'),
+    path('squadre/<int:pk>', views.team_detail, name='squadra'),
     path('competizioni/', views.comps_all, name='competizioni'),
-    path('competizione/<int:pk>', views.comp_detail, name='competizione'),
+    path('competizioni/<int:pk>', views.comp_detail, name='competizione'),
     path('partita/<int:pk>', views.game_detail, name='partita')
 ]
