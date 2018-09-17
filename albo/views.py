349fc6894e288f26178a377fcf479a7f397bf554
Tommaso A <tommaso.amici@me.com>
2018-09-17 21:51:36 +0200

    black formatting

--- albo/views.py
+++ albo/views.py
@@ -6,44 +6,70 @@ from datetime import datetime
 
 # Create your views here.
 def index(request):
-    trophies = Competition.objects.order_by('-date')
+    trophies = Competition.objects.order_by("-date")
     comps_menu = Competition.objects.filter(date__year=datetime.now().year)
-    return render(request, 'albo/albo.html', {'trophies': trophies, 'comps_menu': comps_menu})
+    return render(
+        request, "albo/albo.html", {"trophies": trophies, "comps_menu": comps_menu}
+    )
 
 
 def squadre(request):
-    teams = Team.objects.order_by('name')
+    teams = Team.objects.order_by("name")
     comps_menu = Competition.objects.filter(date__year=datetime.now().year)
-    return render(request, 'albo/rose.html', {'teams': teams, 'comps_menu': comps_menu})
+    return render(request, "albo/rose.html", {"teams": teams, "comps_menu": comps_menu})
 
 
 def team_detail(request, pk):
     team = get_object_or_404(Team, pk=pk)
-    players = Player.objects.filter(team=team).order_by('toorder', 'name')
-    trophies = Competition.objects.filter(winner=team).order_by('date')
+    players = Player.objects.filter(team=team).order_by("toorder", "name")
+    trophies = Competition.objects.filter(winner=team).order_by("date")
     comps_menu = Competition.objects.filter(date__year=datetime.now().year)
-    return render(request, 'albo/squadra.html', {'team': team, 'players': players, 'trophies': trophies, 'comps_menu': comps_menu})
+    return render(
+        request,
+        "albo/squadra.html",
+        {
+            "team": team,
+            "players": players,
+            "trophies": trophies,
+            "comps_menu": comps_menu,
+        },
+    )
 
 
 def game_detail(request, pk):
     game = get_object_or_404(Game, pk=pk)
     comps_menu = Competition.objects.filter(date__year=datetime.now().year)
-    return render(request, 'albo/partita.html', {'game': game, 'comps_menu': comps_menu})
+    return render(
+        request, "albo/partita.html", {"game": game, "comps_menu": comps_menu}
+    )
 
 
 def comps_all(request):
-    comps = Competition.objects.order_by('date')
+    comps = Competition.objects.order_by("date")
     comps_menu = Competition.objects.filter(date__year=datetime.now().year)
-    return render(request, albo/comps_all.html, {'comps': comps, 'comps_menu': comps_menu})
+    return render(
+        request, albo / comps_all.html, {"comps": comps, "comps_menu": comps_menu}
+    )
 
 
 def comp_detail(request, pk):
     comp = get_object_or_404(Competition, pk=pk)
-    m = Standings.objects.filter(competition=comp).aggregate(Max('matchday'))['matchday__max']
+    m = Standings.objects.filter(competition=comp).aggregate(Max("matchday"))[
+        "matchday__max"
+    ]
     try:
         standings = Standings.objects.get(competition=comp, matchday=m)
     except:
         standings = []
-    games = Game.objects.filter(competition=comp).order_by('matchday')
+    games = Game.objects.filter(competition=comp).order_by("matchday")
     comps_menu = Competition.objects.filter(date__year=datetime.now().year)
-    return render(request, 'albo/comp.html', {'comp': comp, 'games': games, 'comps_menu': comps_menu, 'standings': standings})
+    return render(
+        request,
+        "albo/comp.html",
+        {
+            "comp": comp,
+            "games": games,
+            "comps_menu": comps_menu,
+            "standings": standings,
+        },
+    )
